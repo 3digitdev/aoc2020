5f779c751cadf50ac398d347440c34bb374569d7
Max Bussiere <max@3digit.dev>
2020-12-05 00:14:29 -0600

    Added Day 2 for Elixir

--- elixir/lib/aoc.ex
+++ elixir/lib/aoc.ex
@@ -1,12 +1,13 @@
 defmodule AOC do
     def run do
         Day1.run()
+        Day2.run()
     end
 end
 
 defmodule Input do
   def readFile(filename) do
     {:ok, lines} = File.read(filename)
-    lines |> String.split |> Enum.filter(fn(x) -> x != "" end)
+    lines |> String.split("\n") |> Enum.filter(fn(x) -> x != "" end)
   end
 end
---
+++ elixir/lib/day2.ex
@@ -0,0 +1,43 @@
+defmodule Day2 do
+    def run do
+        IO.puts "---- DAY 2 ----"
+        lines = Input.readFile("../inputs/day2.txt")
+        part1(lines)
+        part2(lines)
+    end
+
+    defp deconstruct(line) do
+        # Map.values returns them alphabetically by capture group
+        ~r/(?<a>\d+)-(?<b>\d+) (?<c>\w): (?<d>.*)/
+            |> Regex.named_captures(line)
+            |> Map.values
+    end
+
+    defp charCountValid(passLine) do
+        [min, max, char, pass] = deconstruct(passLine)
+        [min, max] = [min, max] |> Enum.map(&String.to_integer/1)
+        {:ok, char} = Regex.compile(char)
+        case char |> Regex.scan(pass) |> Enum.count do
+            n when min <= n and n <= max -> 1
+            _ -> 0
+        end
+    end
+
+    defp part1(lines) do
+        IO.puts "There are #{lines |> Enum.map(&charCountValid/1) |> Enum.sum} valid passwords"
+    end
+
+    defp xor(a, b) do (a and !b) or (!a and b) end
+
+    defp charLocValid(passLine) do
+        [one, two, char, pass] = deconstruct(passLine)
+        [one, two] 
+            |> Enum.map(&(String.to_integer(&1) - 1))
+            |> Enum.map(&(String.at(pass, &1) == char))
+            |> Enum.reduce(false, &xor/2)
+    end
+
+    defp part2(lines) do
+        IO.puts "There are #{lines |> Enum.map(&charLocValid/1) |> Enum.filter(&(&1)) |> Enum.count} valid passwords"
+    end
+end
