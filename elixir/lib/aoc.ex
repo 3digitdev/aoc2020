132c61b7b8a18d23acab2681a18e4d21c31d990c
Max Bussiere <max@3digit.dev>
2020-12-04 22:12:11 -0600

    added elixir to the mix

---
+++ elixir/lib/aoc.ex
@@ -0,0 +1,42 @@
+defmodule AOC do
+  defmodule Day1 do
+    def run do
+        IO.puts("---- DAY 1 ----")
+        nums = Input.readFile("../inputs/day1.txt")
+            |> Enum.map(&(String.to_integer(&1)))
+        part1(nums)
+        part2(nums)
+    end
+
+    defp part1(nums) do
+        case nums 
+            |> Enum.filter(&(nums |> Enum.member?(2020 - &1)))
+            |> List.first do
+            :nil -> IO.puts("No answers")
+            n -> IO.puts(n * (2020 - n))
+        end
+    end
+
+    defp part2(nums) do
+        result = nums 
+            |> Comb.combinations(3) 
+            |> Stream.filter(fn(c) -> Enum.reduce(c, &+/2) == 2020 end)
+            |> Stream.map(fn(s) -> Enum.reduce(s, &*/2) end)
+            |> Enum.take(1)
+            |> List.first
+        case result do
+            :nil -> IO.puts "No result"
+            n -> IO.puts n
+        end
+    end
+  end
+end
+
+defmodule Input do
+  def readFile(filename) do
+    {:ok, lines} = File.read(filename)
+    lines |> String.split |> Enum.filter(fn(x) -> x != "" end)
+  end
+end
+
+AOC.Day1.run()
---
+++ src/day5.rs
@@ -0,0 +1,16 @@
+use crate::util::read_file;
+use fancy_regex::Regex;
+
+pub fn run() {
+    println!("---- DAY 5 ----");
+    part_1();
+    part_2();
+}
+
+fn part_1() {
+    let lines = read_file("inputs/day5.txt");
+}
+
+fn part_2() {
+    let lines = read_file("inputs/day5.txt");
+}
