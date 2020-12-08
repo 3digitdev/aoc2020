367410f70421dd4e589a6d411cf54eca066797cb
Max Bussiere <max@3digit.dev>
2020-12-08 01:04:55 -0600

    Day 8 in Elixir done

--- elixir/lib/aoc.ex
+++ elixir/lib/aoc.ex
@@ -2,7 +2,7 @@ defmodule AOC do
     def run do
         days = [
             Day1, Day2, Day3, Day4, Day5,
-            Day6, Day7
+            Day6, Day7, Day8
         ]
         tasks = days |> Enum.map(&Task.async(fn -> &1.run() end))
         tasks |> Enum.each(&Task.await(&1))
@@ -14,4 +14,12 @@ defmodule Input do
     {:ok, lines} = File.read("../inputs/#{day}.txt")
     lines |> String.split("\n")
   end
+end
+
+defmodule Util do
+    # Pass-through debugger for mid-pipe inspections
+    def debug(thing, prefix \\ "") do
+        IO.puts "#{prefix}: #{inspect thing}"
+        thing
+    end
 end
---
+++ elixir/lib/day8.ex
@@ -0,0 +1,68 @@
+defmodule Day8 do
+    def run do
+        all_cmds = Input.readFile("day8")
+            |> Enum.with_index 
+            |> Enum.map(&parse_cmd/1) 
+            |> Enum.sort(&(&1.line < &2.line))
+        part1(all_cmds)
+        part2(all_cmds)
+    end
+
+    defmodule Cmd do
+        defstruct [:line, :cmd, :num, :used]
+    end
+
+    defp parse_cmd({cmd, idx}) do
+        m = Regex.named_captures(~r/(?<cmd>acc|jmp|nop) (?<num>[\+\-]\d+)/, cmd)
+        %Cmd{
+            line: idx,
+            cmd: Map.get(m, "cmd"), 
+            num: m |> Map.get("num") |> String.to_integer, 
+            used: false
+        }
+    end
+
+    defp execute_cmds(all_cmds) do
+        {ended, _, global, _} = 0..1
+            |> Stream.cycle
+            |> Enum.reduce_while({false, [], 0, 0}, fn(_, {ended, used, global, idx}) ->
+                cmd = all_cmds |> Enum.at(idx)
+                cond do
+                    cmd == nil and idx == Enum.count(all_cmds) ->
+                        {:halt, {true, used, global, idx}}
+                    Enum.member?(used, cmd.line) ->
+                        {:halt, {ended, used, global, idx}}
+                    true ->
+                        case cmd.cmd do
+                            "nop" -> {:cont, {ended, used ++ [cmd.line], global, idx + 1}}
+                            "jmp" -> {:cont, {ended, used ++ [cmd.line], global, idx + cmd.num}}
+                            "acc" -> {:cont, {ended, used ++ [cmd.line], global + cmd.num, idx + 1}}
+                            i -> raise "ERROR #{i}"
+                        end
+                end
+            end)
+        {ended, global}
+    end
+
+    defp part1(all_cmds) do
+        {_, global} = execute_cmds(all_cmds)
+        IO.puts "8-1: Global value is [#{global}]"
+    end
+
+    def swap_cmd(c), do: %Cmd{c | cmd: if c.cmd == "jmp" do "nop" else "jmp" end }
+
+    defp part2(all_cmds) do
+        cmd = all_cmds
+            |> Enum.filter(&(&1.cmd != "acc"))
+            |> Enum.drop_while(fn(cmd) ->
+                new_cmds = List.update_at(all_cmds, cmd.line, &swap_cmd/1)
+                {ended, _} = execute_cmds(new_cmds)
+                !ended
+            end)
+            |> List.first
+        {_, global} = all_cmds 
+            |> List.update_at(cmd.line, &swap_cmd/1)
+            |> execute_cmds
+        IO.puts "8-2: Global value is [#{global}]"
+    end
+end
