ba66b2f6b113a8aa38641273359fed979b5a68cf
Max Bussiere <max@3digit.dev>
2020-12-13 15:58:49 -0600

    merged day 11 solutions

--- lib/day11.ex
+++ lib/day11.ex
@@ -1,94 +1,75 @@
 defmodule Day11 do
     def run do
         seats = Input.readFile("day11")
+            |> Enum.map(&(for <<c <- &1>>, do: <<c>>))  # split binary string into chars
+        seats = for {row, r} <- Enum.with_index(seats),
+            {col, c} <- Enum.with_index(row),
+            into: %{},
+            do: {{r, c}, col}
         [&part1/1, &part2/1] 
             |> Enum.map(&Task.async(fn -> &1.(seats) end))
-            |> Enum.each(&Task.await(&1, 20_000))
+            |> Enum.each(&Task.await(&1, 10_000))
     end
 
-    defp at(seats, r, c), do: seats |> Enum.at(r) |> Enum.at(c)
-
-    defp in_bounds?(seats, r, c) do
-        r >= 0 and r < Enum.count(seats) and c >= 0 and c < seats |> Enum.at(r) |> Enum.count
+    defp neighbors(seats, {r, c}) do
+        (for ro <- -1..1,
+            co <- -1..1,
+            {ro, co} != {0, 0},
+            do: Map.get(seats, {r + ro, c + co}, ".")
+        ) |> Enum.reduce(0, &(&2 + (if &1 == "#" do 1 else 0 end)))
     end
 
-    # Used for Part 1
-    defp neighbors(seats, row, col) do
-        (for r <- -1..1, 
-            c <- -1..1, 
-            in_bounds?(seats, row + r, col + c),
-            {r, c} != {0, 0},
-            do: {row + r, col + c})
-            |> Enum.reduce(0, fn({r, c}, full) ->
-                full + (if seats |> at(r,c) == "#" do 1 else 0 end)
-            end)
-    end
-
-    # Used for Part 2
-    defp los_neighbors(seats, row, col) do
-        (for r <- -1..1,
-            c <- -1..1,
-            {r, c} != {0, 0},
-            do: {r, c}
-        )
-            |> Enum.reduce(0, fn({r, c}, total) ->
-                {sub, _} = Enum.reduce_while(
-                    Stream.cycle(0..1), {total, 1}, fn(_, {subtotal, add}
-                ) ->
-                    if in_bounds?(seats, row + (r * add), col + (c * add)) do
-                        case seats |> at(row + (r * add), col + (c * add)) do
-                            "#" -> {:halt, {1, add}}
-                            "L" -> {:halt, {0, add}}
-                            _ -> {:cont, {0, add + 1}}
-                        end
-                    else
-                        {:halt, {0, add}}
+    defp los_neighbors(seats, {r, c}) do
+        (for ro <- -1..1,
+            co <- -1..1,
+            {ro, co} != {0, 0},
+            do: {ro, co}
+        ) |> Enum.reduce(0, fn({ro, co}, total) ->
+            {sub, _} = Stream.cycle(0..1)
+                |> Enum.reduce_while({total, 1}, fn(_, {subtotal, add}) ->
+                    case Map.get(seats, {ro * add + r, co * add + c}) do
+                        "#" -> {:halt, {1, add}}
+                        "L" -> {:halt, {0, add}}
+                        :nil -> {:halt, {0, add}}
+                        _ -> {:cont, {0, add + 1}}
                     end
                 end)
-                total + sub
-            end)
-    end
-
-    defp toggle_seat(seats, r, c) do
-        List.update_at(seats, r, fn(row) ->
-            List.update_at(row, c, fn(s) ->
-                case s do
-                    "L" -> "#"
-                    "#" -> "L"
-                    a -> a
-                end
-            end)
+            total + sub
         end)
     end
 
-    defp count_taken(seats), do: Enum.reduce(seats, 0, fn(r, total) -> total + (r |> Enum.filter(&(&1 == "#")) |> Enum.count) end)
+    defp check_toggle(seats, coords, live, new, chk) do
+        if chk.(live) do Map.update!(seats, coords, fn(_) -> new end) else seats end
+    end
+
+    defp count_taken(seats) do
+        Enum.count(for v <- Map.values(seats), v == "#", do: v)
+    end
 
     defp find_stability(part, seats, limit, neighbor_fn) do
-        seats = seats |> Enum.map(fn(line) -> line |> String.split("") |> Enum.filter(&(&1 != "")) end)
-        indexes = for r <- 0..Enum.count(seats) - 1, c <- 0..(seats |> Enum.at(0) |> Enum.count) - 1, do: {r, c}
         Enum.reduce_while(Stream.cycle(0..1), seats, fn(_, iter_state) ->
-            nxt_state = Enum.reduce(indexes, iter_state, fn({row, col}, new_state) ->
-                live = neighbor_fn.(iter_state, row, col)
-                case iter_state |> at(row, col) do
-                    "L" -> if live == 0 do toggle_seat(new_state, row, col) else new_state end
-                    "#" -> if live >= limit do toggle_seat(new_state, row, col) else new_state end
+            next = Enum.reduce(Map.keys(iter_state), iter_state, fn(coords, new_state) ->
+                live = neighbor_fn.(iter_state, coords)
+                case Map.get(iter_state, coords) do
+                    "L" -> check_toggle(new_state, coords, live, "#", &(&1 == 0))
+                    "#" -> check_toggle(new_state, coords, live, "L", &(&1 >= limit))
                     _ -> new_state
                 end
             end)
-            if nxt_state == iter_state do
-                IO.puts "#{part}: There are [#{count_taken(nxt_state)}] occupied seats"
-                {:halt, nxt_state}
+            if next == iter_state do
+                IO.puts "#{part}: There are [#{count_taken(next)}] occupied seats"
+                {:halt, next}
             else
-                {:cont, nxt_state}
+                {:cont, next}
             end
         end)
     end
 
     defp part1(seats) do
-        find_stability("11-1", seats, 4, &neighbors/3)
+        find_stability("11-1", seats, 4, &neighbors/2)
     end
 
     defp part2(seats) do
-        find_stability("11-2", seats, 5, &los_neighbors/3)
+        find_stability("11-2", seats, 5, &los_neighbors/2)
     end
 end
